dfa5b4a487bd0353d0152449f913e88fc9544224
smileycat <liaocrong@outlook.com>
2021-01-04 22:23:55 +1100

    feat: added interactive mode

--- src/replaceTexts.js
+++ src/replaceTexts.js
@@ -1,12 +1,17 @@
 #!/usr/bin/env node
-const readline = require('readline-sync');
 const fs = require('fs');
+const commander = require('commander');
+const readline = require('readline-sync');
 const pathScanner = require('./utils/pathScanner');
-const args = process.argv.slice(2);
+const program = new commander.Command();
 
-if (!args.length) {
-  console.log('usage: rptxt <file> ...');
-  return;
+program
+  .usage('[options] file ...')
+  .option('-i, --interactive', 'verify before processing each file')
+  .parse(process.argv);
+
+if (program.args.length === 0) {
+  program.help();
 }
 
 // Getting user inputs
@@ -16,6 +21,12 @@ console.log();
 
 // Processing through each provided file
 pathScanner(args, { appName: 'rptxt' }).forEach(file => {
+  // Interactive mode
+  if (program.interactive) {
+    const ans = readline.question(`process ${file} (y/n)? `);
+    if (!ans.trim().toLowerCase().startsWith('y')) return;
+  }
+
   let data = fs.readFileSync(file, 'utf-8');
   let count = (data.match(new RegExp(search, 'g')) || []).length;
   data = data.replace(new RegExp(search, 'g'), replacement);
--- src/s2t.js
+++ src/s2t.js
@@ -1,16 +1,28 @@
 #!/usr/bin/env node
 const fs = require('fs');
+const commander = require('commander');
+const readline = require('readline-sync');
 const OpenCC = require('opencc');
-const opencc = new OpenCC('s2twp.json');
 const pathScanner = require('./utils/pathScanner');
-const args = process.argv.slice(2);
 
-if (!args.length) {
-  console.log('usage: s2t <file> ...');
-  return;
+const opencc = new OpenCC('s2twp.json');
+const program = new commander.Command();
+
+program
+  .usage('[options] file ...')
+  .option('-i, --interactive', 'verify before processing each file')
+  .parse(process.argv);
+
+if (program.args.length === 0) {
+  program.help();
 }
 
-pathScanner(args, { appName: 's2t' }).forEach(file => {
+pathScanner(program.args, { appName: 's2t' }).forEach(file => {
+  // Interactive mode
+  if (program.interactive) {
+    const ans = readline.question(`process ${file} (y/n)? `);
+    if (!ans.trim().toLowerCase().startsWith('y')) return;
+  }
   const content = fs.readFileSync(file);
   const converted = opencc.convertSync(content);
 
